8cbc7217353ca200ad8ae5b8ecee50e3bb5d15e1
joaovq <joaovq@gmail.com>
2017-03-04 19:29:49 -0300

    updated gradles with new dependecies

--- domain/src/main/java/com/sample/joao/domain/task/TaskDomainMapper.java
+++ domain/src/main/java/com/sample/joao/domain/task/TaskDomainMapper.java
@@ -1,9 +1,15 @@
 package com.sample.joao.domain.task;
 
+import com.sample.joao.common.Mapper;
+import com.sample.joao.data.task.TaskDBModel;
 
 /**
  * Mapper usado para transformar {@link TaskDBModel} (na camada de dados) em um
  * {@link TaskDomainModel}
  */
-public class TaskDomainMapper {
+public class TaskDomainMapper implements Mapper<TaskDBModel,TaskDomainModel> {
+    @Override
+    public TaskDomainModel map(TaskDBModel model) {
+        return null;
+    }
 }
